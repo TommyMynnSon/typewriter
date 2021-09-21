8ae80d9eec535165dac42bed3b28055a6f16c41b
Tommy Mynn Son <tommy.m.son@gmail.com>
2021-09-21 14:23:04 -0700

    Implement animateSentence function in typewriter.js

---
+++ typewriter.js
@@ -0,0 +1,22 @@
+const animateSentence = (sentence) => {
+  let timer = 0;
+  let current = 0;
+
+  for (const char of sentence) {
+    timer += 75;
+    current++;
+
+    if (current === sentence.length) {
+      setTimeout(() => {
+        process.stdout.write(char + '\n');
+      }, timer);
+    } else {
+      setTimeout(() => {
+        process.stdout.write(char);
+      }, timer);
+    }
+  }
+}
+
+// Test/driver code.
+animateSentence('My favorite foods are:\n1) Fish & Chips\n2) Pizza\n3) Fried Chicken');
